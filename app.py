6e3aa015efcebb513f467fcd19dccb97f0eb352c
renato.figueiredo1109@gmail.com <Lab05_etec@etec.com>
2026-05-15 14:08:40 -0300

    Incrementação do PUT DELETE

--- app.py
+++ app.py
@@ -1,20 +1,77 @@
 from flask import Flask,jsonify
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
 
 app=Flask(__name__)
+ 
+CORS(app)
 
-books=[
+usuarios=[
     {"id":1,"name":"João"},
     {"id":2 ,"name":"Jonas "},
     {"id":3 ,"name":"Jerimundo "}
 ]
 
-@app.route ("/books", methods=["GET"])
-def home():
-    return jsonify({"mensagem: ":"API de Usuarios"})
 
-@app.route("/", methods=["GET"])
-def listar_books():
-    return jsonify(books)
+@app.route("/usuarios", methods=["GET"])
+def buscar_usuarios():
 
-if __name__ == "__main__":
-    app.run(port=5001)
+    return jsonify(usuarios)
+
+
+@app.route("/usuarios", methods=["POST"])
+def cadastrar_usuario():
+
+    novo_usuario = request.json
+
+    usuarios.append(novo_usuario)
+
+    return jsonify({
+        "mensagem": "Usuário cadastrado!",
+        "usuario": novo_usuario
+    })
+
+
+
+@app.route("/usuarios/<int:id>", methods=["PUT"])
+def atualizar_usuario(id):
+
+    dados = request.json
+
+    for usuario in usuarios:
+
+        if usuario["id"] == id:
+
+            usuario["nome"] = dados["nome"]
+
+            return jsonify({
+                "mensagem": "Usuário atualizado!",
+                "usuario": usuario
+            })
+
+    return jsonify({
+        "mensagem": "Usuário não encontrado"
+    })
+
+
+
+@app.route("/usuarios/<int:id>", methods=["DELETE"])
+def excluir_usuario(id):
+
+    for usuario in usuarios:
+
+        if usuario["id"] == id:
+
+            usuarios.remove(usuario)
+
+            return jsonify({
+                "mensagem": "Usuário removido!"
+            })
+
+    return jsonify({
+        "mensagem": "Usuário não encontrado"
+    })
+
+
+app.run(debug=True)
